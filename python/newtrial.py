6b1035fe4561f1156a6e329db65003d959c996fe
Cheryl Dunn <dunnc@gvsu.edu>
2019-06-04 10:31:33 -0400

    to update index for new project

---
+++ python/newtrial.py
@@ -0,0 +1,12 @@
+print("This is my third project")
+print("Python can calculate gross profit and gross margin")
+num1 = 1000000
+num2 = 600000
+sales = num1
+cogs = num2
+gross_profit = sales - cogs
+print("Sales", sales)
+print("COGS", cogs)
+print("Gross Profit", gross_profit)
+gross_margin = (sales - cogs)/sales
+print("Gross Margin", gross_margin)
